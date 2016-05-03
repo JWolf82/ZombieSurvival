03cf6db7adccc4ca4cafa55d0040674cc80e8a97
JWolf82 <jlwolf82@gmail.com>
2016-05-03 21:49:03 +0000

    Post OOP

--- main.js
+++ main.js
@@ -1,54 +1,71 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image('ground', 'assets/images/background.gif');
+    game.load.image('ground', 'assets/images/background.png');
     game.load.spritesheet('zombie', 'assets/sprites/zombie.gif', 65, 80);
     game.load.spritesheet('soldier', 'assets/sprites/soldier.gif', 75, 80, 2)
+
+    
+    
 }
 
 
 var button;
-var zombie;
-var zombie2;
-var zombie3;
-var soldier;
-var soldierHealth;
-var zombieHealth;
-var newZombieHealth
-var bullets = 5;
 var button1;
 var buttonLabel1;
+var pDamage;
+
 
 function create() {
     
-    //background
-    game.add.sprite(0,0, 'ground')
+ //background
+    game.add.sprite(-600,0, 'ground')
     
     
-    //Soldier
-    soldier = game.add.sprite(500, 440, 'soldier')
-    soldier.frame = 1;
-    soldierHealth = 100
-
-   
+//Objects
+    function Player(name, animate, health, bullets, sprite, dmg){
+        this.name = name,
+        this.animate = animate,
+        this.health = health,
+        this.bullets = bullets,
+        this.sprite = sprite,
+        this.dmg = dmg 
+    }
     
-    //Damage
-    //Soldier
+    var Soldier = new Player(
+        "",
+        "",
+        100,
+        5,
+        game.add.sprite(500, 440, 'soldier'),
+        pDamage
+        )
+        
+    function Npc(health, sprite){
+        this.health = health,
+        this.sprite = sprite
+    }
+    
+//Start Horde
+    var horde = []
+    
+    for(var x = 0; x<=4; x++){
+        var z = [50, 86, 146, 187, 215]
+        var Zombie = new Npc(
+        25,
+        game.add.sprite(z[x], 440, 'zombie')
+        )
+       
+//horde array 
+     horde.push(Zombie)
+      
+    };
+  
     
     
     
-    
-    
-    //Zombies
-    zombie = game.add.sprite(125, 440, 'zombie');
-    zombie2 = game.add.sprite(175, 440, 'zombie');
-    zombie3 = game.add.sprite(145, 440, 'zombie');
-    zombieHealth = 25
-    
-    
-    
-    
-    //draw hud
+        
+//GUI/HUD
 var drawActionMenuBorder;
 var bmd = game.add.bitmapData(352, 152);
 bmd.ctx.beginPath();
@@ -68,52 +85,47 @@ drawnActionMenu.anchor.setTo(0.5, 0.5);
     
 //HealthCount
     var style = { font: "40px Arial", fill: "#fff", align: "center" };
-    var healthText = game.add.text(520, 35, soldierHealth, style);
+    var healthText = game.add.text(520, 35, Soldier.health, style);
     
 //buttons
     var buttonStyle = { font: "32px Arial", fill: "#ffffff" };
     
-    button1 = game.add.button( 650, 10, null, shootGun);
+    button1 = game.add.button( 650, 10, null, playerShoot);
     buttonLabel1 = game.make.text( 0, 0, "Shoot", buttonStyle)
     button1.addChild( buttonLabel1 );
 
 
-    var button2 = game.add.button( 650, 60, null, reloadGun);
-    var buttonLabel2 = game.make.text( 0, 0, "Reload", buttonStyle)
-    button2.addChild( buttonLabel2 );
+    // var button2 = game.add.button( 650, 60, null, reloadGun);
+    // var buttonLabel2 = game.make.text( 0, 0, "Reload", buttonStyle)
+    // button2.addChild( buttonLabel2 );
 
-    
-}
-
-///shoot gun
- function shootGun () {
-    var shoot = soldier.animations.add('shoot');
-    soldier.animations.play('shoot', 4, false);
-    if(zombieHealth > 0){
-       newZombieHealth = zombieHealth - 5
-       zombieHealth = newZombieHealth
-       if(zombieHealth <= 0){
-        zombie.kill()
-        }
-        
-        
-    }
-    
-    if(bullets>1){
-        var magazine = bullets - 1
-        bullets = magazine
-    } else {
-        button1.visible = false
+//Methods
+    function playerShoot(){
+       var newZombieHealth;
+       var lastZ = horde.length -1
+       pDamage = game.rnd.integerInRange(12, 25);
+        newZombieHealth = horde[lastZ].health - pDamage
+         horde[lastZ].health = newZombieHealth
+       if(horde[lastZ].health <= 0){
+          horde[lastZ].sprite.kill()
+          horde.splice(lastZ, 1)
+       }  
+       
+       
+       
     }
 }
 
-function reloadGun(){
-    button1.visible = true
-    bullets = 5
-    zombieHealth = 25
-}
 
+
+        
+    
+
+    
+    
 function update(){
     
     
 }
+
+   

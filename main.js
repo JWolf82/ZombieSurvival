3863e938365d1cc677665133e62ce55bdcff107b
JWolf82 <jlwolf82@gmail.com>
2016-05-04 17:19:50 +0000

    OOP V2 w/ death animation

--- main.js
+++ main.js
@@ -2,26 +2,29 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, cr
 var Phaser;
 function preload() {
     game.load.image('ground', 'assets/images/background.png');
-    game.load.spritesheet('zombie', 'assets/sprites/zombie.png', 44, 64, 9);
     game.load.spritesheet('soldier', 'assets/sprites/Soldier.gif', 64, 64, 7)
-
+    game.load.spritesheet('zombie', 'assets/sprites/zombiecombo.png', 66, 64, 25)
+    game.load.audio('machinegun', 'assets/sounds/machinegun.mp3');
+    // game.load.audio('background', 'assets/sounds/electrictiger.mp3')
     
 }
 
-
+//Globals
 var button;
 var button1;
 var buttonLabel1;
 var pDamage;
 
 
-
+//Begin Create
 function create() {
     
     
- //background
+//Background Image and Audio
     game.add.sprite(-600,0, 'ground')
-    
+    // var back = game.add.audio('background');
+    //  back.play();
+    //  back.volume = .015
     
 //Objects
     function Player(name, animate, health, bullets, sprite, dmg){
@@ -42,31 +45,31 @@ function create() {
         pDamage
         )
  
-        
     function Npc(health, sprite){
         this.health = health,
         this.sprite = sprite
     }
+     
     
-//zombie animation
-//Start Horde
+//Start Horde/Create new NPC
     var horde = []
     
     for(var x = 0; x<=4; x++){
-        
-        var z = [50, 86, 146, 187, 215]
-        var Zombie = new Npc(
-        25,
-        game.add.sprite(z[x], 440, 'zombie')
-        )
-        
-            var shamble = Zombie.sprite.animations.add('shamble');
-Zombie.sprite.animations.play('shamble', 10, true);
-game.add.tween(Zombie.sprite).to({ x: game.width }, 20000, Phaser.Easing.Linear.None, true);
-       
-//horde array 
-     horde.push(Zombie)
-      
+            var z = [-215, -186, -146, -87, -45]
+            var Zombie = new Npc(
+            25,
+            game.add.sprite(z[x], 440, 'zombie')
+            )
+
+     //Zombie Animation Shamble  !!!!!!!!!!
+        var shamble = Zombie.sprite.animations.add('shamble', [0,1,2,3,4,5,6,7,8]);
+        Zombie.sprite.animations.play('shamble', 10, true);
+        game.add.tween(Zombie.sprite).to({ x: game.width }, 15000, Phaser.Easing.Linear.None, true);
+
+
+
+//Horde array 
+        horde.push(Zombie)
     };
   
     
@@ -89,44 +92,61 @@ drawnActionMenu.anchor.setTo(0.5, 0.5);
     bmd.ctx.fill();
     drawnActionMenu = game.add.sprite(650, 50, bmd);
     drawnActionMenu.anchor.setTo(0.5, 0.5);
-    
-//HealthCount
+
+    //HealthCount
     var style = { font: "40px Arial", fill: "#fff", align: "center" };
     var healthText = game.add.text(520, 35, Soldier.health, style);
     
-//buttons
+    //Buttons
     var buttonStyle = { font: "32px Arial", fill: "#ffffff" };
-    
     button1 = game.add.button( 650, 10, null, playerShoot);
     buttonLabel1 = game.make.text( 0, 0, "Shoot", buttonStyle)
     button1.addChild( buttonLabel1 );
-
 
     // var button2 = game.add.button( 650, 60, null, reload);
     // var buttonLabel2 = game.make.text( 0, 0, "Reload", buttonStyle)
     // button2.addChild( buttonLabel2 );
 
-//Methods
 
-    
+//Methods
     //Fire Weapon w/ Damage
     function playerShoot(){
-        var shoot = Soldier.sprite.animations.add('shoot');
-    Soldier.sprite.animations.play('shoot', 25, false);
-       var newZombieHealth;
-       var lastZ = horde.length -1
-       pDamage = game.rnd.integerInRange(12, 25);
+        //animation
+            var shoot = Soldier.sprite.animations.add('shoot');
+            Soldier.sprite.animations.play('shoot', 25, false);
+        //Sound
+             var bang = game.add.audio('machinegun');
+             bang.play();
+    
+        //Health-Damage change
+        var newZombieHealth;
+        var lastZ = horde.length -1
+        pDamage = game.rnd.integerInRange(12, 30);
         newZombieHealth = horde[lastZ].health - pDamage
-         horde[lastZ].health = newZombieHealth
-       if(horde[lastZ].health <= 0){
-          horde[lastZ].sprite.kill()
-          horde.splice(lastZ, 1)
-          console.log(horde)
-       } 
+        horde[lastZ].health = newZombieHealth
+               
+        //Zed animation death       
+        if(horde[lastZ].health <= 0){
+         var pointX = Zombie.sprite.x -13
+         var pointY = Zombie.sprite.y
+         console.log(pointX, pointY)
+         var zDie = this.game.add.sprite(pointX, pointY, 'zombie')
+            zDie.animations.add('zDie', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14,15,16,17,18,19,20,21,22,23,24], false);
+            zDie.animations.play('zDie');
+                horde[lastZ].sprite.kill()
+                horde.splice(lastZ, 1)
+                console.log(horde)
+         } 
+   
+   
+   
     }
+    
+
+
+//Dump Bin
 
     // function reload(){
-        
     //     if(Soldier.bullets>0){
     //     var magazine = Soldier.bullets - 1
     //     Soldier.bullets = magazine
@@ -140,9 +160,9 @@ drawnActionMenu.anchor.setTo(0.5, 0.5);
     
     
     
-//end create    
+   
 }
-
+//End create 
 
 
         
